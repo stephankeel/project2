f9dec80fc283483657732a0c3f7b640b1918b990
Stephan Keel <git@zopf.ch>
2016-11-26 15:09:09 +0100

    clear message on focus or key down event

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -12,21 +12,21 @@ import {GenericService} from './generic.service';
 export class LoginComponent {
     username: string;
     password: string;
-    loginInfo: string;
+    message: string;
     loggedOut: boolean = true;
 
     constructor(private genericService: GenericService,
                 private router: Router) {
         this.loggedOut = !genericService.loggedIn();
     }
-    
+
     doLogin(): void {
         this.genericService.login(this.username, this.password).then(user => {
-            this.loginInfo = 'welcome';
+            this.message = 'welcome';
             this.loggedOut = false;
             this.router.navigate(['/dashboard']);
         }).catch(error => {
-            this.loginInfo = 'login failure: ' + (error.message || error);
+            this.message = 'login failure: ' + (error.message || error);
         });
     }
 
@@ -35,5 +35,8 @@ export class LoginComponent {
         this.loggedOut = true;
     }
 
+    clearMessage(): void {
+        this.message = null;
+    }
 }
 
