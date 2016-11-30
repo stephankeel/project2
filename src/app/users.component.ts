041534ccdc1b330ec7de73a3843f04e08ed3db99
Stephan Keel <git@zopf.ch>
2016-11-30 23:08:36 +0100

    Add, remove and delete of users

--- src/app/users.component.ts
+++ src/app/users.component.ts
@@ -39,23 +39,41 @@ export class UsersComponent {
 
     selectUser(user: User) {
         this.selectedUser = user;
-        this.user = user;
+        this.user =  new User(this.selectedUser.id, this.selectedUser.firstname, this.selectedUser.lastname, this.selectedUser.username, this.selectedUser.password);
         this.passwordConfirmation = this.user.password;
     }
 
     doAddOrUpdate(): void {
         if (this.user) {
             if (this.user.id) {
-                // TODO: Update
+                this.genericService.updateUser(this.user).then(() => {
+                    this.users[this.users.indexOf(this.selectedUser)] = this.user;
+                    this.selectedUser = null;
+                    this.user = null;
+                }).catch(error => {
+                    this.message = 'Update user failed with ' + (error.message || error )
+                });
             } else {
-                // TODO: Add
+                this.genericService.addUser(this.user).then(user => {
+                    this.users.push(user);
+                    this.selectedUser = null;
+                    this.user = null;
+                }).catch(error => {
+                    this.message = 'Add user failed with ' + (error.message || error )
+                });
             }
         }
     }
 
     doDelete(): void {
         if (this.user && this.user.id) {
-            // TODO delete user
+            this.genericService.deleteUser(this.user).then(() => {
+                this.users = this.users.filter(user => user.id != this.user.id);
+                this.selectedUser = null;
+                this.user = null;
+            }).catch(error => {
+                this.message = 'Delete user failed with ' + (error.message || error )
+            });
         }
     }
 
