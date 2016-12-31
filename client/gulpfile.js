31e53be226b459c826fe613069edbf1aad428fed
David Leuenberger <david.leuenberger@hsr.ch>
2016-12-31 12:09:29 +0100

    add shell:true to every spawn or spawnSync call

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,9 +5,9 @@ var spawnSync = require('child_process').spawnSync;
 gulp.task('default', ['watch-and-build']);
 
 gulp.task('watch-and-build', function () {
-  spawnSync('./node_modules/angular-cli/bin/ng', ['build', '-w'], {stdio: 'inherit'});
+  spawnSync('.\\node_modules\\.bin\\ng', ['build', '-w'], {stdio: 'inherit', shell: true});
 });
 
 gulp.task('watch-and-build-without-logs', function () {
-  spawnSync('./node_modules/angular-cli/bin/ng', ['build', '-w']);
+  spawnSync('.\\node_modules\\.bin\\ng', ['build', '-w'], {shell: true});
 });
