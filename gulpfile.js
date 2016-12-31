c3ae3813cd3d65644c351d92a9a44b0903a3c71c
David Leuenberger <david.leuenberger@hsr.ch>
2016-12-31 12:40:43 +0100

    make file resolving platform independent

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
 var spawn = require('child_process').spawn;
 var spawnSync = require('child_process').spawnSync;
+const gulpExe = require.resolve('./node_modules/.bin/gulp');
 
 gulp.task('default', ['run-without-client-logs']);
 gulp.task('run-with-client-logs', ['watch-and-build-client', 'run-server']);
 gulp.task('run-without-client-logs', ['watch-and-build-client-without-logs', 'run-server']);
 gulp.task('run-server', function () {
-    spawn(".\\node_modules\\.bin\\gulp", [], {stdio: 'inherit', cwd: 'server', shell: true});
+    spawn(gulpExe, [], {stdio: 'inherit', cwd: 'server', shell: true});
 });
 gulp.task('watch-and-build-client', function () {
-    spawn('.\\node_modules\\.bin\\gulp', [], {stdio: 'inherit', cwd: 'client', shell: true});
+    spawn(gulpExe, [], {stdio: 'inherit', cwd: 'client', shell: true});
 });
 gulp.task('watch-and-build-client-without-logs', function () {
-    spawn('.\\node_modules\\.bin\\gulp', ['watch-and-build-without-logs'], {stdio: 'inherit', cwd: 'client', shell: true});
+    spawn(gulpExe, ['watch-and-build-without-logs'], {stdio: 'inherit', cwd: 'client', shell: true});
 });
 
 gulp.task('install', ['npm-install-server', 'npm-install-client']);
