80691131c4a769730fdfb261fec22d9966a6cef3
David Leuenberger <david.leuenberger@hsr.ch>
2017-01-06 17:22:25 +0100

    reformating server

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -5,76 +5,76 @@ import express = require('express');
 import {IUserModel, User} from '../models/user.model';
 
 export function addUser(req: express.Request, res: express.Response, next: express.NextFunction) {
-        let user: IUserModel = new User(req.body);
-        let jsonBody: string = JSON.stringify(req.body);
-        logger.info(`create user: ${jsonBody}`);
-        user.save((err: any, addedUser: IUserModel) => {
-            if (err) {
-                res.status(500).json({error: `error creating user ${user.username}. ${err}`});
-            } else {
-                // set the user.id to the _id provided by the db
-                addedUser.id = addedUser._id;
-                logger.trace(`created user successfully, id: ${addedUser.id}`);
-                res.status(201).json({data: addedUser});
-            }
-        });
-    };
+    let user: IUserModel = new User(req.body);
+    let jsonBody: string = JSON.stringify(req.body);
+    logger.info(`create user: ${jsonBody}`);
+    user.save((err: any, addedUser: IUserModel) => {
+        if (err) {
+            res.status(500).json({error: `error creating user ${user.username}. ${err}`});
+        } else {
+            // set the user.id to the _id provided by the db
+            addedUser.id = addedUser._id;
+            logger.trace(`created user successfully, id: ${addedUser.id}`);
+            res.status(201).json({data: addedUser});
+        }
+    });
+};
 
 export function updateUser(req: express.Request, res: express.Response, next: express.NextFunction) {
-        let id = req.params.id;
-        let jsonBody: string = JSON.stringify(req.body);
-        logger.info(`update user [${id}]: ${jsonBody}`);
-        User.findById(id, (err: any, user: IUserModel) => {
-            if (err) {
-                res.status(404).json({error: `user ${id} not found. ${err}`});
-            } else {
-                // copy the properties
-                user.id = req.body.id;
-                user.lastname = req.body.lastname;
-                user.firstname = req.body.firstname;
-                user.type = req.body.type;
-                user.username = req.body.username;
-                user.password = req.body.password;
-                // save the updated user
-                user.save((err: any, updatedUser: IUserModel) => {
-                    if (err) {
-                        res.status(500).json({error: `error updating user ${id}. ${err}`});
-                    } else {
-                        logger.trace('updated user successfully');
-                        res.json({data: updatedUser});
-                    }
-                });
-            }
-        });
-    }
+    let id = req.params.id;
+    let jsonBody: string = JSON.stringify(req.body);
+    logger.info(`update user [${id}]: ${jsonBody}`);
+    User.findById(id, (err: any, user: IUserModel) => {
+        if (err) {
+            res.status(404).json({error: `user ${id} not found. ${err}`});
+        } else {
+            // copy the properties
+            user.id = req.body.id;
+            user.lastname = req.body.lastname;
+            user.firstname = req.body.firstname;
+            user.type = req.body.type;
+            user.username = req.body.username;
+            user.password = req.body.password;
+            // save the updated user
+            user.save((err: any, updatedUser: IUserModel) => {
+                if (err) {
+                    res.status(500).json({error: `error updating user ${id}. ${err}`});
+                } else {
+                    logger.trace('updated user successfully');
+                    res.json({data: updatedUser});
+                }
+            });
+        }
+    });
+}
 
 export function getAllUsers(req: express.Request, res: express.Response, next: express.NextFunction) {
-        User.find((err: any, users: IUserModel[]) => {
-            if (err) {
-                res.status(404).json({error: `error retrieving users. ${err}`});
-            } else {
-                // set the user.id to the _id provided by the db
-                users.forEach((user) => user.id = user._id);
-                logger.debug(`found ${users.length} users`);
-                res.json({data: users});
-            }
-        });
-    }
+    User.find((err: any, users: IUserModel[]) => {
+        if (err) {
+            res.status(404).json({error: `error retrieving users. ${err}`});
+        } else {
+            // set the user.id to the _id provided by the db
+            users.forEach((user) => user.id = user._id);
+            logger.debug(`found ${users.length} users`);
+            res.json({data: users});
+        }
+    });
+}
 
 export function getUser(req: express.Request, res: express.Response, next: express.NextFunction) {
-        logger.debug(`get user ${req.params.id}`);
-        let ref = {_id: req.params.id};
-        User.findById(ref, (err: any, user: IUserModel) => {
-            if (err) {
-                res.status(404).json({error: `error retrieving user ${ref._id}. ${err}`});
-            } else {
-                // set the user.id to the _id provided by the db
-                user.id = user._id;
-                logger.debug(`found user ${req.params.id}: ${JSON.stringify(user)}`);
-                res.json({data: user});
-            }
-        });
-    }
+    logger.debug(`get user ${req.params.id}`);
+    let ref = {_id: req.params.id};
+    User.findById(ref, (err: any, user: IUserModel) => {
+        if (err) {
+            res.status(404).json({error: `error retrieving user ${ref._id}. ${err}`});
+        } else {
+            // set the user.id to the _id provided by the db
+            user.id = user._id;
+            logger.debug(`found user ${req.params.id}: ${JSON.stringify(user)}`);
+            res.json({data: user});
+        }
+    });
+}
 
 export function deleteUser(req: express.Request, res: express.Response, next: express.NextFunction) {
     logger.info(`delete user ${req.params.id}`);
--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -4,13 +4,14 @@ import {IUser} from '../entities/user.interface';
 import {UserType} from '../entities/user-type';
 
 export interface IUserModel extends IUser, Document {
-};
+}
+;
 
 export let UserSchema = new Schema({
     id: String,
     firstname: String,
     lastname: String,
-    type: {type: Number, required: true, min:UserType.GUEST, max:UserType.ADMIN},
+    type: {type: Number, required: true, min: UserType.GUEST, max: UserType.ADMIN},
     username: {type: String, required: true, minlength: 4, unique: true},
     password: {type: String, required: true, minlength: 4}
 }, {
@@ -18,16 +19,16 @@ export let UserSchema = new Schema({
 });
 
 /*
-UserSchema.pre('save', (next) => {
-    // TODO: Pre save validation
-    next();
-});
-*/
+ UserSchema.pre('save', (next) => {
+ // TODO: Pre save validation
+ next();
+ });
+ */
 
-export const User:Model<IUserModel> = model<IUserModel>('User', UserSchema);
+export const User: Model<IUserModel> = model<IUserModel>('User', UserSchema);
 
 export function initAdmin() {
-    let username:String = 'admin';
+    let username: String = 'admin';
     let selector = {'username': username}
     User.find(selector, (err, users) => {
         if (users.length) {
@@ -35,14 +36,14 @@ export function initAdmin() {
             return;
         }
 
-        let user:IUserModel = new User();
+        let user: IUserModel = new User();
         user.firstname = 'admin';
         user.lastname = 'admin';
         user.type = UserType.ADMIN;
         user.username = 'admin';
         user.password = '123456';
         logger.info(`creating admin user: ${JSON.stringify(user)}`);
-        user.save((err:any, adminUser:IUserModel) => {
+        user.save((err: any, adminUser: IUserModel) => {
             if (err) {
                 throw new Error(err);
             } else {
