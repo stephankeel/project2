d71b3c144373d9884d7848a1a3c6511b710d9769
Stephan Keel <git@zopf.ch>
2017-01-19 22:31:16 +0100

    use baseUrsl instead of url (relative)

--- server/routes/authorization.ts
+++ server/routes/authorization.ts
@@ -8,11 +8,11 @@ import {UserType} from '../entities/user-type';
 export function requiresAdmin(req: express.Request, res: express.Response, next: express.NextFunction) {
   let valid: boolean = req.user && (req.user.type == UserType.ADMIN);
   if (valid) {
-    logger.info(`user is authorized for ${req.url}`);
+    logger.info(`user is authorized for ${req.baseUrl}`);
     next();
   } else {
-    logger.error(`user ${req.user.username} is authorized for ${req.url}`);
-    res.status(403).json({error: `not authorized to use ${req.url}`});
+    logger.error(`user ${req.user.username} is authorized for ${req.baseUrl}`);
+    res.status(403).json({error: `not authorized to use ${req.baseUrl}`});
   }
 
 }
