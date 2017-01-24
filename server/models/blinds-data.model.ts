5e0cdab9b4335e4f02a893cdd366842d651278a9
David Leuenberger <david.leuenberger@hsr.ch>
2017-01-24 15:08:45 +0100

    fix bug in blinds-data model, discovered by added unittest

--- server/models/blinds-data.model.ts
+++ server/models/blinds-data.model.ts
@@ -8,7 +8,12 @@ export interface IBlindsDataDocument extends IBlindsData, Document {
 let BlindsDataSchema = new Schema({
   deviceId: {type: String, required: true},
   timestamp: {type: Number, required: true},
-  state: {type: Number, required: true, min: blindsStateValues[0], max: blindsStateValues[blindsStateValues.length]},
+  state: {
+    type: Number,
+    required: true,
+    min: blindsStateValues[0],
+    max: blindsStateValues[blindsStateValues.length - 1]
+  },
 }, {
   versionKey: false, // avoids __v, i.e. the version key
 });
