a425c13c3ddfc5c2bf1ac8ff023760b3149a359a
David Leuenberger <david.leuenberger@hsr.ch>
2017-01-25 07:51:38 +0100

    fix filename of generic.data-controller.ts

--- server/controllers/temperature-data.controller.ts
+++ server/controllers/temperature-data.controller.ts
@@ -1,7 +1,7 @@
 import {TemperatureDataModel, ITemperatureDataDocument} from "../models/temperature-data.model";
 import {ResponseContainer, ResponseCollectionContainer} from "../wire/com-container";
 import {ITemperatureData} from "../entities/data.interface";
-import {GenericDataController} from "./generic.data.controller";
+import {GenericDataController} from "./generic.data-controller";
 import express = require('express');
 
 export class TemperatureDataController extends GenericDataController<ITemperatureData, ITemperatureDataDocument> {
