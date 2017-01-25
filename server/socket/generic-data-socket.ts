f587eb59b4256a91b0716944175f5f3a98837cc8
David Leuenberger <david.leuenberger@hsr.ch>
2017-01-25 16:11:03 +0100

    rename sensorId to deviceId

--- server/socket/generic-data-socket.ts
+++ server/socket/generic-data-socket.ts
@@ -3,20 +3,20 @@ import {logger} from '../utils/logger';
 /**
  * The GenericDataSocket broadcasts values from one sensor.
  *
- * It creates a new socket.io namespace with the name '/<namespacePrefix>/<sensorId>'.
+ * It creates a new socket.io namespace with the name '/<namespacePrefix>/<deviceId>'.
  * All values are send to every client which is connected to this namespace.
  * The send object is a ISocketItem with the action 'update' and item of type of the namespacePrefix with a date and value.
  */
 export class GenericDataSocket<T> {
   public namespace: any;
 
-  constructor(private io: SocketIO.Server, private namespacePrefix: string, private sensorId: string) {
-    this.namespace = this.io.of(`/${namespacePrefix}/${sensorId}`);
+  constructor(private io: SocketIO.Server, private namespacePrefix: string, private deviceId: string) {
+    this.namespace = this.io.of(`/${namespacePrefix}/${deviceId}`);
     this.namespace.on("connection", (socket: any) => {
-      logger.info(`Socket.IO: Client on ${namespacePrefix} sensor(${this.sensorId}) connected`);
+      logger.info(`Socket.IO: Client on ${namespacePrefix} sensor(${this.deviceId}) connected`);
       this.listen(socket);
     });
-    logger.info(`Socket.IO: namespace /${namespacePrefix}/${this.sensorId} created`);
+    logger.info(`Socket.IO: namespace /${namespacePrefix}/${this.deviceId} created`);
   }
 
   public broadcast(value: T) {
