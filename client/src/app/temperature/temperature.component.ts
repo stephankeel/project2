26736fd5ff6b32b3f44dfba013478a32426c1111
David Leuenberger <david.leuenberger@hsr.ch>
2017-02-09 18:45:07 +0100

    use async pipe

--- client/src/app/temperature/temperature.component.ts
+++ client/src/app/temperature/temperature.component.ts
@@ -1,12 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Router}    from '@angular/router';
 import {ClientSocketService} from '../remote/client-socket.service';
-import {ITemperatureData} from "../../../../server/entities/data.interface";
 import {GenericService} from "../remote/generic.service";
 import {AuthHttp} from "angular2-jwt";
-import {AuthenticationService} from "../remote/authentication.service";
 import {ITemperatureDevice} from "../../../../server/entities/device.interface";
-import {TemperatureService} from "./temperature.service";
 
 @Component({
   selector: 'app-temperature',
@@ -15,13 +12,15 @@ import {TemperatureService} from "./temperature.service";
 })
 export class TemperatureComponent implements OnInit {
 
-  constructor(private router: Router, private socketService: ClientSocketService,
-              private authHttp: AuthHttp, private temperatureService: TemperatureService) {
+  public temperatureService: GenericService<ITemperatureDevice>;
+
+  constructor(private router: Router, private socketService: ClientSocketService, private authHttp: AuthHttp) {
   }
 
   ngOnInit() {
-    this.temperatureService = new TemperatureService(this.socketService, this.authHttp);
-    this.temperatureService.init();
+    this.temperatureService = new GenericService<ITemperatureDevice>(this.authHttp,
+      this.socketService, "/api/devices/temperature", "/temperature");
+    this.temperatureService.getAll();
   }
 
   backClicked(): void {
