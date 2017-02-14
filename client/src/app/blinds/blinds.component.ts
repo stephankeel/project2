c8095ac846e24effca543f4d8fcb13e64cdc0488
Stephan Keel <git@zopf.ch>
2017-02-14 18:48:26 +0100

    continued

--- client/src/app/blinds/blinds.component.ts
+++ client/src/app/blinds/blinds.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthHttp} from "angular2-jwt";
+
+import {GenericService} from "../remote/generic.service";
+import {ClientSocketService} from "../remote/client-socket.service";
+import {BlindsDevice, blindsDevicesInfo, Port, portName} from '../device-pool';
+import {GenericDataService} from "../remote/generic-data.service";
 
 @Component({
   selector: 'app-blinds',
@@ -7,9 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlindsComponent implements OnInit {
 
-  constructor() { }
+  headerTitle: string = `${blindsDevicesInfo.displayName}-STEUERUNG`;
+  devices: BlindsDevice[] = [];
+  selectedDevice: BlindsDevice;
+  genericService: GenericService<BlindsDevice>;
+  statusText: string = 'stopped';
+  message: string;
+
+  constructor(private router: Router,
+              private socketService: ClientSocketService, private authHttp: AuthHttp) {
+  }
 
   ngOnInit() {
+    this.genericService = new GenericService<BlindsDevice>(this.authHttp, this.socketService, "/api/devices/blinds", "/blinds");
+    this.genericService.items.subscribe(devices => {
+      this.devices = devices.toArray();
+      this.selectedDevice = null;
+    }, error => this.message = error.toString());
+    this.genericService.getAll();
+  }
+
+  ngOnDestroy() {
+    this.genericService.disconnect();
+  }
+
+  backClicked(): void {
+    this.router.navigate(['/dashboard']);
+  }
+
+  selectDevice(device: BlindsDevice) {
+    this.clearMessage();
+    this.selectedDevice = device;
+  }
+
+  clearMessage(): void {
+    this.message = null;
+  }
+
+  keyUpAction(): void {
+
+  }
+
+  keyDownAction(): void {
+
+  }
+
+  stopAction(): void {
+
   }
 
 }
