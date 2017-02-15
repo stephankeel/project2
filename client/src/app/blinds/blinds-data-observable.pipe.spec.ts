67ac3da8095b6b3c79f93dc2116805b68e39f9aa
Stephan Keel <git@zopf.ch>
2017-02-15 19:14:36 +0100

    bind the blinds component to the server data. Using pipes to convert data.

---
+++ client/src/app/blinds/blinds-data-observable.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BlindsDataObservablePipe } from './blinds-data-observable.pipe';
+
+describe('BlindsDataObservablePipe', () => {
+  it('create an instance', () => {
+    const pipe = new BlindsDataObservablePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- client/src/app/blinds/blinds.component.ts
+++ client/src/app/blinds/blinds.component.ts
@@ -1,25 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthHttp} from "angular2-jwt";
+import {Observable} from "rxjs";
 
 import {GenericService} from "../remote/generic.service";
 import {ClientSocketService} from "../remote/client-socket.service";
 import {BlindsDevice, blindsDevicesInfo, Port, portName} from '../device-pool';
 import {GenericDataService} from "../remote/generic-data.service";
+import {IBlindsDevice} from "../../../../server/entities/device.interface";
+import {IBlindsData} from "../../../../server/entities/data.interface";
+import {BlindsDataObservablePipe} from './blinds-data-observable.pipe';
+import {BlindsDataFormatterPipe} from './blinds-data-formatter.pipe';
 
 @Component({
   selector: 'app-blinds',
   templateUrl: './blinds.component.html',
-  styleUrls: ['./blinds.component.scss']
+  styleUrls: ['./blinds.component.scss'],
+  providers: [BlindsDataObservablePipe, BlindsDataFormatterPipe]
 })
 export class BlindsComponent implements OnInit {
 
-  headerTitle: string = `${blindsDevicesInfo.displayName}-STEUERUNG`;
-  devices: BlindsDevice[] = [];
-  selectedDevice: BlindsDevice;
-  genericService: GenericService<BlindsDevice>;
-  statusText: string = 'stopped';
-  message: string;
+  private headerTitle: string = `${blindsDevicesInfo.displayName}-STEUERUNG`;
+  private devices: BlindsDevice[] = [];
+  private selectedDevice: BlindsDevice;
+  private genericService: GenericService<BlindsDevice>;
+  private statusText: string = 'stopped';
+  private message: string;
+  private dataServices: Map<BlindsDevice, GenericDataService<IBlindsData>> = new Map<BlindsDevice, GenericDataService<IBlindsData>>();
+  private devicesState: Map<BlindsDevice, Observable<IBlindsData>> = new Map<BlindsDevice, Observable<IBlindsData>>();
 
   constructor(private router: Router,
               private socketService: ClientSocketService, private authHttp: AuthHttp) {
@@ -36,6 +44,24 @@ export class BlindsComponent implements OnInit {
 
   ngOnDestroy() {
     this.genericService.disconnect();
+    this.devices.forEach(device => this.releaseDevice(device));
+  }
+
+  subscribeDevice(device: BlindsDevice): void {
+    let dataService: GenericDataService<IBlindsData> = new GenericDataService<IBlindsData>(this.authHttp, this.socketService, '/api/data/blinds', '/blinds', device.id);
+    this.dataServices.set(device, dataService);
+    this.devicesState.set(device, dataService.lastItem);
+//    dataService.getAll();
+    dataService.getLatest();
+  }
+
+  releaseDevice(device: BlindsDevice): void {
+    let dataService:  GenericDataService<IBlindsData> = this.dataServices.get(device);
+    if (dataService) {
+      dataService.disconnect();
+      this.dataServices.delete(device);
+      this.devicesState.delete(device);
+    }
   }
 
   backClicked(): void {
@@ -44,6 +70,10 @@ export class BlindsComponent implements OnInit {
 
   selectDevice(device: BlindsDevice) {
     this.clearMessage();
+    if (this.selectedDevice) {
+      this.releaseDevice(this.selectedDevice);
+    }
+    this.subscribeDevice(device);
     this.selectedDevice = device;
   }
 
