2a9b72c1b71b73d2a9d4a48271ab966d10ce2ccd
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-03 09:20:53 +0100

    use hashed passwords in db

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -2,6 +2,7 @@ import {Logger, getLogger} from '../utils/logger';
 import {Document, Schema, Model, model} from 'mongoose';
 import {IUser} from '../entities/user.interface';
 import {UserType} from '../entities/user-type';
+import {hash, compare} from 'bcrypt';
 
 const LOGGER: Logger = getLogger('UserModel');
 
@@ -19,12 +20,20 @@ let UserSchema = new Schema({
   versionKey: false, // avoids __v, i.e. the version key
 });
 
-/*
- UserSchema.pre('save', (next) => {
- // TODO: Pre save validation
- next();
- });
- */
+UserSchema.pre(`save`, function (next) {
+  let user: IUserDocument = this;
+
+// only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  hash(user.password, 8, function (err, hash) {
+    if (err) return next(err);
+
+    // override the cleartext password with the hashed one
+    user.password = hash;
+    next();
+  });
+});
 
 export const UserModel: Model<IUserDocument> = model<IUserDocument>('User', UserSchema);
 
