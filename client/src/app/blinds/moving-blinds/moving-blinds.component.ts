2fa6595d75ce374d38aa8f3ba8b476b4b24a5cbe
Stephan Keel <git@zopf.ch>
2017-03-06 21:28:11 +0100

    prototype of all binds pane

---
+++ client/src/app/blinds/moving-blinds/moving-blinds.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-moving-blinds',
+  templateUrl: './moving-blinds.component.html',
+  styleUrls: ['./moving-blinds.component.scss']
+})
+export class MovingBlindsComponent{
+
+  @Input() percentageDown: number;
+  @Input() name?: string;
+
+  constructor() { }
+
+}
