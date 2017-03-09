77272ab999512278f4443bb7d821e940e30d5fef
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-09 18:39:01 +0100

    rename file

--- client2/src/app/router/app-route.module.ts
+++ client2/src/app/router/app-route.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {DashboardComponent} from "../dashboard/dashboard.component";
+import {AuthGuard} from "../auth/auth-guard.service";
+import {LoginComponent} from "../login/login.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', canActivate: [AuthGuard], component: DashboardComponent},
 ];
 
 @NgModule({
