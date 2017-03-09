4ac0f0d844a4a5c2035760f4e836ca964a415ee6
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-09 07:24:58 +0100

    add empty dashboard, add simple sidemenu, add material

--- client2/src/app/app.component.ts
+++ client2/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private router: Router) {
+
+  }
 }
--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {MaterialModule} from "@angular/material";
+import {AppRouteModule} from "./router/app-route-module";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    MaterialModule,
+    HttpModule,
+    AppRouteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
