d4087fd5e90176e766d232e35ca0c30fb24ec040
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-10 07:44:54 +0100

    use footer in the dashboard module too

--- client2/src/app/+dashboard/dashboard.module.ts
+++ client2/src/app/+dashboard/dashboard.module.ts
@@ -5,12 +5,14 @@ import {DashboardRouterModule} from "./dashboard-router/dashboard-router.module"
 import {AuthGuard} from "../auth/auth-guard.service";
 import {DeviceOverviewComponent} from "./device-overview/device-overview.component";
 import {MaterialModule} from "@angular/material";
+import {FooterModule} from "../footer/footer.module";
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRouterModule,
     MaterialModule,
+    FooterModule,
   ],
   declarations: [
     DashboardComponent,
