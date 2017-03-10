b0dd554d5362a044091905279e31f3eac4eb883e
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-10 07:15:18 +0100

    move dashboard component into dashboard folder

--- client2/src/app/dashboard/dashboard-router/dashboard-router.module.ts
+++ client2/src/app/dashboard/dashboard-router/dashboard-router.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterModule, Routes} from "@angular/router";
-import {DashboardComponent} from "../dashboard.component";
+import {DashboardComponent} from "../dashboard/dashboard.component";
 import {AuthGuard} from "../../auth/auth-guard.service";
 import {DeviceOverviewComponent} from "../device-overview/device-overview.component";
 
--- client2/src/app/dashboard/dashboard.module.ts
+++ client2/src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {DashboardComponent} from "./dashboard.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 import {DashboardRouterModule} from "./dashboard-router/dashboard-router.module";
 import {AuthGuard} from "../auth/auth-guard.service";
 import {DeviceOverviewComponent} from "./device-overview/device-overview.component";
