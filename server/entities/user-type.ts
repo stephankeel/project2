b3dda6db1a2124536949379f772bdd0abd5a8c4f
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-12 17:57:06 +0100

    user-tpye ergänzt, redesign notwendig?

--- server/entities/user-type.ts
+++ server/entities/user-type.ts
@@ -11,3 +11,8 @@ export const UserTypeString: string[] = ['Guest', 'Standard', 'Admin'];
 export function userTypeAsString(type: UserType) {
   return UserTypeString[type];
 }
+
+export const UserTypesArray: any[] = [{key: UserType.GUEST, value: userTypeAsString(UserType.GUEST)},
+  {key: UserType.STANDARD, value: userTypeAsString(UserType.STANDARD)},
+  {key: UserType.ADMIN, value: userTypeAsString(UserType.ADMIN)},
+];
