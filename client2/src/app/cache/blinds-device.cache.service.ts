8a1efed2b735e9db862bbbb911b0abf29e83a26d
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-15 20:20:30 +0100

    add caches

---
+++ client2/src/app/cache/blinds-device.cache.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@angular/core";
+import {AuthHttp} from "angular2-jwt";
+import {GenericeCacheService} from "./generic.cache.service";
+import {ClientSocketService} from "../remote/client-socket.service";
+import {NotificationService} from "../notification/notification.service";
+import {IBlindsDevice} from "../../../../server/entities/device.interface";
+
+@Injectable()
+export class BlindsDeviceCacheService extends GenericeCacheService<IBlindsDevice> {
+  constructor(private authHttp: AuthHttp, private socketService1: ClientSocketService, private notificationService1: NotificationService) {
+    super(authHttp, socketService1, notificationService1, "/api/devices/blinds", "/blinds");
+  }
+}
