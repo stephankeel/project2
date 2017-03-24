148e7a74226a114308c1557b6a4db7521f1a2dbd
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-24 19:33:18 +0100

    use new generic-data-cache

--- client/src/app/+dashboard/analog-devices/pipes/pipes.module.ts
+++ client/src/app/+dashboard/analog-devices/pipes/pipes.module.ts
@@ -4,6 +4,7 @@ import {ValuePipe} from '../pipes/value.pipe';
 import {TimestampPipe} from '../pipes/timestamp.pipe';
 import {TimestampFormatterPipe} from '../pipes/timestamp-formatter.pipe';
 import {ValueSplitPipe} from '../pipes/value-split.pipe';
+import { NamePipe } from './name.pipe';
 
 @NgModule({
   imports: [
@@ -14,12 +15,14 @@ import {ValueSplitPipe} from '../pipes/value-split.pipe';
     TimestampPipe,
     TimestampFormatterPipe,
     ValueSplitPipe,
+    NamePipe,
   ],
   exports: [
     ValuePipe,
     TimestampPipe,
     TimestampFormatterPipe,
     ValueSplitPipe,
+    NamePipe,
   ],
   providers: [
   ]
