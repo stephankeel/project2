0c5d7bd1b6f333326160926a52ad7457b5e7fd09
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-26 15:45:25 +0200

    add test for MovingBlindsComponent

--- client/src/app/+dashboard/blinds/moving-blinds/moving-blinds.component.ts
+++ client/src/app/+dashboard/blinds/moving-blinds/moving-blinds.component.ts
@@ -1,15 +1,15 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-moving-blinds',
   templateUrl: './moving-blinds.component.html',
   styleUrls: ['./moving-blinds.component.scss']
 })
-export class MovingBlindsComponent{
+export class MovingBlindsComponent {
 
   @Input() percentageDown: number;
   @Input() name?: string;
 
-  constructor() { }
-
+  constructor() {
+  }
 }
