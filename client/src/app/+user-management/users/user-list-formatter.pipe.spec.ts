bda02dea8f7b58fa6fa6b68b48bb2a415fe356fb
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-26 15:15:58 +0200

    add tests to UserListFormatterPipe

--- client/src/app/+user-management/users/user-list-formatter.pipe.spec.ts
+++ client/src/app/+user-management/users/user-list-formatter.pipe.spec.ts
@@ -1,8 +1,23 @@
-import { UserListFormatterPipe } from './user-list-formatter.pipe';
+import {UserListFormatterPipe} from "./user-list-formatter.pipe";
+import {IUser} from "../../../../../server/entities/user.interface";
 
 describe('UserListFormatterPipe', () => {
   it('create an instance', () => {
     const pipe = new UserListFormatterPipe();
     expect(pipe).toBeTruthy();
+  });
+  it('test username, firstname and lastname attributes of IUser', () => {
+    const pipe = new UserListFormatterPipe();
+    let user: IUser = {username: "username1", firstname: "firstname1", lastname: "lastname1"};
+    expect(pipe.transform(user)).toEqual("firstname1 lastname1 (username1)");
+  });
+  it('test empty IUser', () => {
+    const pipe = new UserListFormatterPipe();
+    let user: IUser = {};
+    expect(pipe.transform(user)).toEqual("undefined undefined (undefined)");
+  });
+  it('test null', () => {
+    const pipe = new UserListFormatterPipe();
+    expect(pipe.transform(null)).toBeNull();
   });
 });
