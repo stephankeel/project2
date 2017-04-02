cc6b2895a9037b1b811d42e65bb7fc46d5fab677
David Leuenberger <david.leuenberger@hsr.ch>
2017-04-02 20:01:23 +0200

    add unittests

--- client/src/app/+dashboard/info/info.component.spec.ts
+++ client/src/app/+dashboard/info/info.component.spec.ts
@@ -1,15 +1,38 @@
 /* tslint:disable:no-unused-variable */
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
-import {InfoComponent} from './info.component';
+import {InfoComponent} from "./info.component";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Component, Input} from "@angular/core";
+import {CommonRestService} from "../../remote/common-rest.service";
+import {Observable} from "rxjs";
 
 describe('InfoComponent', () => {
   let component: InfoComponent;
   let fixture: ComponentFixture<InfoComponent>;
 
+  let routerSpy: Router;
+  let commonRestSpy: CommonRestService;
+
   beforeEach(async(() => {
+
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+    commonRestSpy = jasmine.createSpyObj<CommonRestService>('CommonRestService', ['getInfo']);
+    (<jasmine.Spy>commonRestSpy.getInfo).and.returnValue(Observable.create(observer => {
+      observer.next({title: "title", nodeVersion: "1", cpus: [{model: 'cpuModel'}], totalMem: "1000000000", freeMem: "1000000"});
+      observer.next({title: "title", nodeVersion: "1", cpus: [{model: 'cpuModel'}], totalMem: "100", freeMem: "100000"});
+    }));
+
     TestBed.configureTestingModule({
-      declarations: [InfoComponent]
+      declarations: [
+        InfoComponent,
+        MockListHeaderComponent,
+      ],
+      providers: [
+        {provide: Router, useValue: routerSpy},
+        {provide: ActivatedRoute, useValue: null},
+        {provide: CommonRestService, useValue: commonRestSpy},
+      ]
     })
       .compileComponents();
   }));
@@ -23,4 +46,32 @@ describe('InfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('check appListHeader fields', () => {
+    const appListHeader = fixture.debugElement.children[0];
+    expect(appListHeader.context.title).toBe('INFORMATION');
+    expect(appListHeader.context.backlink).toBe('/dashboard');
+    expect(appListHeader.context.showBack).toBeTruthy();
+    expect(appListHeader.context.showCreate).toBeUndefined();
+    expect(appListHeader.context.disableCreate).toBeUndefined();
+    expect(appListHeader.context.showShowAll).toBeUndefined();
+  });
+
 });
+
+@Component({
+  selector: 'app-list-header',
+  template: '<div></div>',
+})
+class MockListHeaderComponent {
+  @Input() title: string;
+  @Input() backlink: string;
+  @Input() showBack: boolean;
+  @Input() showCreate: boolean;
+  @Input() disableCreate: boolean;
+  @Input() showShowAll: boolean;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
+}
+
--- client/src/app/+dashboard/password-change/password-change.component.spec.ts
+++ client/src/app/+dashboard/password-change/password-change.component.spec.ts
@@ -1,14 +1,59 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PasswordChangeComponent} from './password-change.component';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Component, Input} from "@angular/core";
+import {MaterialModule} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ValidatorsModule} from "ng2-validators";
+import {PasswordChangeRestService} from "../../remote/password-change-rest.service";
+import {NotificationService} from "../../notification/notification.service";
+import {AuthenticationService} from "../../remote/authentication.service";
+import {By} from "@angular/platform-browser";
+import {Observable, ReplaySubject} from "rxjs";
 
 describe('PasswordChangeComponent', () => {
   let component: PasswordChangeComponent;
   let fixture: ComponentFixture<PasswordChangeComponent>;
 
+  let routerSpy: Router;
+  let authenticationServiceSpy: AuthenticationService;
+  let notificationServiceSpy: NotificationService;
+  let passwordChangeRestServiceSpy: PasswordChangeRestService;
+
+  let loginCheckSubject : ReplaySubject<boolean>;
+  let passwordUpdateSubject : ReplaySubject<boolean>;
+
   beforeEach(async(() => {
+
+    loginCheckSubject = new ReplaySubject<boolean>(1);
+    passwordUpdateSubject = new ReplaySubject<boolean>(1);
+
+    authenticationServiceSpy = jasmine.createSpyObj<AuthenticationService>('AuthService', ['loginCheckonly', 'getLoggedInUsername']);
+    (<jasmine.Spy>authenticationServiceSpy.loginCheckonly).and.returnValue(loginCheckSubject);
+    notificationServiceSpy = jasmine.createSpyObj<NotificationService>('NotificationService', ['info']);
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+    passwordChangeRestServiceSpy = jasmine.createSpyObj<PasswordChangeRestService>('PasswordChangeService', ['update']);
+    (<jasmine.Spy>passwordChangeRestServiceSpy.update).and.returnValue(passwordUpdateSubject);
+
     TestBed.configureTestingModule({
-      declarations: [PasswordChangeComponent]
+      imports: [
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ValidatorsModule,
+      ],
+      declarations: [
+        PasswordChangeComponent,
+        MockListHeaderComponent
+      ],
+      providers: [
+        {provide: AuthenticationService, useValue: authenticationServiceSpy},
+        {provide: NotificationService, useValue: notificationServiceSpy},
+        {provide: Router, useValue: routerSpy},
+        {provide: ActivatedRoute, useValue: null},
+        {provide: PasswordChangeRestService, useValue: passwordChangeRestServiceSpy},
+      ]
     })
       .compileComponents();
   }));
@@ -22,4 +67,96 @@ describe('PasswordChangeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('check appListHeader fields', () => {
+    const appListHeader = fixture.debugElement.children[0].children[0];
+    expect(appListHeader.context.title).toBe('Passwort ändern');
+    expect(appListHeader.context.backlink).toBeUndefined();
+    expect(appListHeader.context.showBack).toBeTruthy();
+    expect(appListHeader.context.showCreate).toBeUndefined();
+    expect(appListHeader.context.disableCreate).toBeUndefined();
+    expect(appListHeader.context.showShowAll).toBeUndefined();
+  });
+
+  it('should update password', () => {
+    const currentPwd = fixture.debugElement.query(By.css('input[name=currentPassword]')).nativeElement;
+    const newPwd = fixture.debugElement.query(By.css('input[name=password]')).nativeElement;
+    const confirmedPwd = fixture.debugElement.query(By.css('input[name=confirmPassword]')).nativeElement;
+    const form = fixture.debugElement.query(By.css('.form'));
+
+    currentPwd.value = '12345678';
+    newPwd.value = '12345678';
+    confirmedPwd.value = '12345678';
+    form.triggerEventHandler('submit', null);
+
+    loginCheckSubject.next(true);
+    passwordUpdateSubject.next(false);
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['../password-confirmation'], {relativeTo: null});
+  });
+
+  it('should update password failed, interal server error', () => {
+    const currentPwd = fixture.debugElement.query(By.css('input[name=currentPassword]')).nativeElement;
+    const newPwd = fixture.debugElement.query(By.css('input[name=password]')).nativeElement;
+    const confirmedPwd = fixture.debugElement.query(By.css('input[name=confirmPassword]')).nativeElement;
+    const form = fixture.debugElement.query(By.css('.form'));
+
+    currentPwd.value = '12345678';
+    newPwd.value = '12345678';
+    confirmedPwd.value = '12345678';
+    form.triggerEventHandler('submit', null);
+
+    loginCheckSubject.next(false);
+
+    expect(notificationServiceSpy.info).toHaveBeenCalledTimes(1);
+  });
+
+  it('should update password failed, current password not ok', () => {
+    const currentPwd = fixture.debugElement.query(By.css('input[name=currentPassword]')).nativeElement;
+    const newPwd = fixture.debugElement.query(By.css('input[name=password]')).nativeElement;
+    const confirmedPwd = fixture.debugElement.query(By.css('input[name=confirmPassword]')).nativeElement;
+    const form = fixture.debugElement.query(By.css('.form'));
+
+    currentPwd.value = '12345678';
+    newPwd.value = '12345678';
+    confirmedPwd.value = '12345678';
+    form.triggerEventHandler('submit', null);
+
+    loginCheckSubject.error(false);
+
+    expect(notificationServiceSpy.info).toHaveBeenCalledTimes(1);
+  });
+
+  it('should update password failed', () => {
+    const currentPwd = fixture.debugElement.query(By.css('input[name=currentPassword]')).nativeElement;
+    const newPwd = fixture.debugElement.query(By.css('input[name=password]')).nativeElement;
+    const confirmedPwd = fixture.debugElement.query(By.css('input[name=confirmPassword]')).nativeElement;
+    const form = fixture.debugElement.query(By.css('.form'));
+
+    currentPwd.value = '12345678';
+    newPwd.value = '12345678';
+    confirmedPwd.value = '12345678';
+    form.triggerEventHandler('submit', null);
+
+    loginCheckSubject.next(true);
+    passwordUpdateSubject.error(false);
+
+    expect(notificationServiceSpy.info).toHaveBeenCalledTimes(1);
+  });
 });
+
+@Component({
+  selector: 'app-list-header',
+  template: '<div></div>',
+})
+class MockListHeaderComponent {
+  @Input() title: string;
+  @Input() backlink: string;
+  @Input() showBack: boolean;
+  @Input() showCreate: boolean;
+  @Input() disableCreate: boolean;
+  @Input() showShowAll: boolean;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
+}
