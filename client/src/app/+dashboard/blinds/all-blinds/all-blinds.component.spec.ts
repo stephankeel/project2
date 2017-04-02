b7bea3b3e4bc4429c5283ecc16d06e01110a956b
David Leuenberger <david.leuenberger@hsr.ch>
2017-04-02 16:24:59 +0200

    add client unittests

--- client/src/app/+dashboard/blinds/all-blinds/all-blinds.component.spec.ts
+++ client/src/app/+dashboard/blinds/all-blinds/all-blinds.component.spec.ts
@@ -1,14 +1,60 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AllBlindsComponent} from './all-blinds.component';
+import {Component, Input} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {NotEmptyArrayPipe} from '../../analog-devices/pipes/not-empty-array.pipe';
+import {GenericeCacheService} from '../../../cache/service/generic.cache.service';
+import {GenericDataCacheService} from '../../../cache/service/generic-data-cache.service';
+import {BlindsDeviceCacheService} from '../../../cache/service/blinds-device.cache.service';
+import {BlindDataCacheService} from '../../../cache/service/blinds-data.cache.service';
+import {IDevice} from '../../../../../../server/entities/device.interface';
+import {IBlindsData} from '../../../../../../server/entities/data.interface';
+import {ReplaySubject} from 'rxjs/ReplaySubject';
+import {BlindsState} from '../../../../../../server/entities/blinds-state';
+import {By} from '@angular/platform-browser';
 
 describe('AllBlindsComponent', () => {
   let component: AllBlindsComponent;
   let fixture: ComponentFixture<AllBlindsComponent>;
 
+  let routerSpy: Router;
+  let getAllSubject: ReplaySubject<IDevice>;
+  let getLatestSubject: ReplaySubject<IBlindsData>;
+
+  let device: IDevice;
+  let data: IBlindsData;
+
   beforeEach(async(() => {
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+
+    device = {name: 'b1', id: 1};
+    getAllSubject = new ReplaySubject<IDevice>(1);
+    getAllSubject.next([device]);
+
+    data = {timestamp: 1, deviceId: 1, percentageDown: 20, state: BlindsState.OPENING};
+    getLatestSubject = new ReplaySubject<IBlindsData>(1);
+    getLatestSubject.next(data);
+
+    const deviceCacheServiceSpy = jasmine.createSpyObj<GenericeCacheService<IDevice>>('DeviceCache', ['getAll']);
+    (<jasmine.Spy>deviceCacheServiceSpy.getAll).and.returnValue(getAllSubject);
+    const dataCacheServiceSpy = jasmine.createSpyObj<GenericDataCacheService<IBlindsData, IDevice>>('DataCache', ['getLatestData']);
+    (<jasmine.Spy>dataCacheServiceSpy.getLatestData).and.returnValue(getLatestSubject);
+
     TestBed.configureTestingModule({
-      declarations: [AllBlindsComponent]
+      declarations: [
+        AllBlindsComponent,
+        MockListHeaderComponent,
+        MockAppBlindsButtonsComponent,
+        MockAppMovingBlindsComponent,
+        NotEmptyArrayPipe,
+      ],
+      providers: [
+        {provide: BlindsDeviceCacheService, useValue: deviceCacheServiceSpy},
+        {provide: BlindDataCacheService, useValue: dataCacheServiceSpy},
+        {provide: Router, useValue: routerSpy},
+        {provide: ActivatedRoute, useValue: null},
+      ]
     })
       .compileComponents();
   }));
@@ -22,4 +68,58 @@ describe('AllBlindsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('check appListHeader fields', () => {
+    const appListHeader = fixture.debugElement.children[0];
+    expect(appListHeader.context.title).toBe('Rollladen-Steuerung (alle)');
+    expect(appListHeader.context.backlink).toBe('/dashboard');
+    expect(appListHeader.context.showBack).toBeTruthy();
+    expect(appListHeader.context.showCreate).toBeUndefined();
+    expect(appListHeader.context.disableCreate).toBeUndefined();
+    expect(appListHeader.context.showShowAll).toBeUndefined();
+  });
+
+  it('should execute select when clicked', () => {
+    const firstActionButton = fixture.debugElement.query(By.css('.action--button'));
+    firstActionButton.triggerEventHandler('click', null);
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['../blinds', device.id], {relativeTo: null});
+  });
+
+  it('check first app-moving-blinds component content', () => {
+    const firstAppMovingBlinds = fixture.debugElement.query(By.css('.app-moving-blinds'));
+    expect(firstAppMovingBlinds.context.percentageDown).toBe(data.percentageDown);
+    expect(firstAppMovingBlinds.context.name).toBe(device.name);
+  });
 });
+
+@Component({
+  selector: 'app-list-header',
+  template: '<div></div>',
+})
+class MockListHeaderComponent {
+  @Input() title: string;
+  @Input() backlink: string;
+  @Input() showBack: boolean;
+  @Input() showCreate: boolean;
+  @Input() disableCreate: boolean;
+  @Input() showShowAll: boolean;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
+}
+
+@Component({
+  selector: 'app-blinds-buttons',
+  template: '<div></div>',
+})
+export class MockAppBlindsButtonsComponent {
+}
+
+@Component({
+  selector: 'app-moving-blinds',
+  template: '<div></div>',
+})
+class MockAppMovingBlindsComponent {
+  @Input() name: string;
+  @Input() percentageDown: number;
+}
--- client/src/app/+dashboard/blinds/blinds-buttons/blinds-buttons.component.spec.ts
+++ client/src/app/+dashboard/blinds/blinds-buttons/blinds-buttons.component.spec.ts
@@ -1,15 +1,32 @@
 /* tslint:disable:no-unused-variable */
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
-import {BlindsButtonsComponent} from './blinds-buttons.component';
+import {BlindsButtonsComponent} from "./blinds-buttons.component";
+import {NotificationService} from "../../../notification/notification.service";
+import {BlindsCommandService} from "../../../remote/blinds-command.service";
+import {By} from "@angular/platform-browser";
+import {IBlindsCommand} from "../../../../../../server/entities/blinds-command.interface";
+import {BlindsAction} from "../../../../../../server/entities/blinds-action";
+import {ReplaySubject} from "rxjs";
 
 describe('BlindsButtonsComponent', () => {
   let component: BlindsButtonsComponent;
   let fixture: ComponentFixture<BlindsButtonsComponent>;
 
+  let notificationServiceSpy: NotificationService;
+  let blindsCommandServiceSpy: BlindsCommandService;
+
   beforeEach(async(() => {
+
+    notificationServiceSpy = jasmine.createSpyObj<NotificationService>('NotificationService', ['error']);
+    blindsCommandServiceSpy = jasmine.createSpyObj<BlindsCommandService>('BlindsCommandService', ['command']);
+
     TestBed.configureTestingModule({
-      declarations: [BlindsButtonsComponent]
+      declarations: [BlindsButtonsComponent],
+      providers: [
+        {provide: NotificationService, useValue: notificationServiceSpy},
+        {provide: BlindsCommandService, useValue: blindsCommandServiceSpy},
+      ]
     })
       .compileComponents();
   }));
@@ -17,10 +34,83 @@ describe('BlindsButtonsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BlindsButtonsComponent);
     component = fixture.componentInstance;
+    component.deviceId = '1';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should execute keyUpAction when clicked', () => {
+    const subject = new ReplaySubject<boolean>(1);
+    subject.next(true);
+    (<jasmine.Spy>blindsCommandServiceSpy.command).and.returnValue(subject);
+
+    const keyUpImage = fixture.debugElement.query(By.css('.app-blinds-button-up'));
+    keyUpImage.triggerEventHandler('click', null);
+
+    expect(blindsCommandServiceSpy.command).toHaveBeenCalledWith({id: '1', action: BlindsAction.OPEN});
+    expect(notificationServiceSpy.error).toHaveBeenCalledTimes(0);
+  });
+
+  it('should execute keyUpAction and notif error when clicked', () => {
+    const subject = new ReplaySubject<boolean>(1);
+    subject.error(true);
+    (<jasmine.Spy>blindsCommandServiceSpy.command).and.returnValue(subject);
+
+    const keyUpImage = fixture.debugElement.query(By.css('.app-blinds-button-up'));
+    keyUpImage.triggerEventHandler('click', null);
+
+    expect(blindsCommandServiceSpy.command).toHaveBeenCalledWith({id: '1', action: BlindsAction.OPEN});
+    expect(notificationServiceSpy.error).toHaveBeenCalled();
+  });
+
+  it('should execute keyDownAction when clicked', () => {
+    const subject = new ReplaySubject<boolean>(1);
+    subject.next(true);
+    (<jasmine.Spy>blindsCommandServiceSpy.command).and.returnValue(subject);
+
+    const keyDownImage = fixture.debugElement.query(By.css('.app-blinds-button-down'));
+    keyDownImage.triggerEventHandler('click', null);
+
+    expect(blindsCommandServiceSpy.command).toHaveBeenCalledWith({id: '1', action: BlindsAction.CLOSE});
+    expect(notificationServiceSpy.error).toHaveBeenCalledTimes(0);
+  });
+
+  it('should execute keyDownAction and notif error when clicked', () => {
+    const subject = new ReplaySubject<boolean>(1);
+    subject.error(true);
+    (<jasmine.Spy>blindsCommandServiceSpy.command).and.returnValue(subject);
+
+    const keyDownImage = fixture.debugElement.query(By.css('.app-blinds-button-down'));
+    keyDownImage.triggerEventHandler('click', null);
+
+    expect(blindsCommandServiceSpy.command).toHaveBeenCalledWith({id: '1', action: BlindsAction.CLOSE});
+    expect(notificationServiceSpy.error).toHaveBeenCalled();
+  });
+
+  it('should execute keyStopAction when clicked', () => {
+    const subject = new ReplaySubject<boolean>(1);
+    subject.next(true);
+    (<jasmine.Spy>blindsCommandServiceSpy.command).and.returnValue(subject);
+
+    const keyStopImage = fixture.debugElement.query(By.css('.app-blinds-button-stop'));
+    keyStopImage.triggerEventHandler('click', null);
+
+    expect(blindsCommandServiceSpy.command).toHaveBeenCalledWith({id: '1', action: BlindsAction.STOP});
+    expect(notificationServiceSpy.error).toHaveBeenCalledTimes(0);
+  });
+
+  it('should execute keyStopAction and notif error when clicked', () => {
+    const subject = new ReplaySubject<boolean>(1);
+    subject.error(true);
+    (<jasmine.Spy>blindsCommandServiceSpy.command).and.returnValue(subject);
+
+    const keyStopImage = fixture.debugElement.query(By.css('.app-blinds-button-stop'));
+    keyStopImage.triggerEventHandler('click', null);
+
+    expect(blindsCommandServiceSpy.command).toHaveBeenCalledWith({id: '1', action: BlindsAction.STOP});
+    expect(notificationServiceSpy.error).toHaveBeenCalled();
   });
 });
