f8e23c09607d091e3c942097d371b59d1aca91b3
David Leuenberger <david.leuenberger@hsr.ch>
2017-04-02 21:01:47 +0200

    add unittests

--- client/src/app/footer/footer/footer.component.spec.ts
+++ client/src/app/footer/footer/footer.component.spec.ts
@@ -1,19 +1,27 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {FooterComponent} from './footer.component';
+import {AuthenticationService} from "../../remote/authentication.service";
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async(() => {
+    const authServiceSpy = jasmine.createSpyObj<AuthenticationService>('AuthService', ['getLoggedInUsername']);
+
     TestBed.configureTestingModule({
-      declarations: [FooterComponent]
+      declarations: [FooterComponent],
+      providers: [
+        {provide: AuthenticationService, useValue: authServiceSpy}
+      ]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
+
+
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
