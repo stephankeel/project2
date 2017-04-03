fafd2c74c8207cbc44342c5e0b2450604d50665d
David Leuenberger <david.leuenberger@hsr.ch>
2017-04-03 17:50:04 +0200

    remove unused pipes

--- client/src/app/+dashboard/analog-devices/pipes/pipes.module.ts
+++ client/src/app/+dashboard/analog-devices/pipes/pipes.module.ts
@@ -1,10 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ValuePipe} from '../pipes/value.pipe';
-import {TimestampPipe} from '../pipes/timestamp.pipe';
 import {TimestampFormatterPipe} from '../pipes/timestamp-formatter.pipe';
 import {ValueSplitPipe} from '../pipes/value-split.pipe';
-import {NamePipe} from './name.pipe';
 import {NotEmptyArrayPipe} from './not-empty-array.pipe';
 import {EmptyArrayPipe} from './empty-array.pipe';
 
@@ -13,20 +10,14 @@ import {EmptyArrayPipe} from './empty-array.pipe';
     CommonModule,
   ],
   declarations: [
-    ValuePipe,
-    TimestampPipe,
     TimestampFormatterPipe,
     ValueSplitPipe,
-    NamePipe,
     NotEmptyArrayPipe,
     EmptyArrayPipe,
   ],
   exports: [
-    ValuePipe,
-    TimestampPipe,
     TimestampFormatterPipe,
     ValueSplitPipe,
-    NamePipe,
     NotEmptyArrayPipe,
     EmptyArrayPipe,
   ],
